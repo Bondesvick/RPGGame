8a691b93ef9ae6210814fb6c98cf296ae0873c06
Ugoo <54181133+Bondesvick@users.noreply.github.com>
2020-12-01 00:53:54 +0100

    updated connection sring

--- RPGGame/Startup.cs
+++ RPGGame/Startup.cs
@@ -36,11 +36,9 @@ namespace RPGGame
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x =>
-                x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
-            //services.AddDbContext<DataContext>(x =>
-            //    x.UseSqlServer(Configuration.GetConnectionString("Second")));
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("Second")));
 
             services.AddControllers();
             services.AddAutoMapper(typeof(Startup));
