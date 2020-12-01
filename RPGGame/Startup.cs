fda15741095c50fdf52e7c37d614b8123d3cc235
Ugoo <54181133+Bondesvick@users.noreply.github.com>
2020-12-01 01:07:18 +0100

    few changes

--- RPGGame/Startup.cs
+++ RPGGame/Startup.cs
@@ -33,7 +33,7 @@ namespace RPGGame
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        //This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
